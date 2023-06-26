e13ac9064742caf5d826f7e494439c4aabba0bbb
Juma <juma.musa@student.moringaschool.com>
2023-06-27 02:16:46 +0300

    done

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,7 +1,9 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
-
+      t.integer :taxi_id
+      t.integer :passenger_id
+      # additional columns...
       t.timestamps
     end
   end
